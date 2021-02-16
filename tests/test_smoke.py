2c84e7409aeceb5b3baac4c52ef4a96df717b58d
Nitarshan Rajkumar <git@nitarshan.com>
2021-02-16 18:02:32 -0500

    Clean up test_smoke

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -6,16 +6,21 @@ import numpy as np
 
 from source.config import Config, SystemType, HParams, OptimizerType, NLPSolverType, IntegrationOrder
 from source.optimizers import get_optimizer
+from source.systems import IndirectFHCS
 
 
 # Test that experiments run without raising exceptions
 class SmokeTest(unittest.TestCase):
-  def test_smoke(self):
+  def setUp(self):
     jax.config.update("jax_enable_x64", True)
+
+  def test_smoke(self):
     for system in SystemType:
       for optimizer in OptimizerType:
+        if optimizer == OptimizerType.FBSM and not issubclass(system.value, IndirectFHCS):
+          continue
         with self.subTest(system=system, optimizer=optimizer):
-          hp = HParams(system=system, optimizer=optimizer, order=IntegrationOrder.CONSTANT)
+          hp = HParams(system=system, optimizer=optimizer, order=IntegrationOrder.LINEAR, intervals=20, ipopt_max_iter=100)
           cfg = Config(verbose=True, plot_results=True)
           random.seed(hp.seed)
           np.random.seed(hp.seed)
