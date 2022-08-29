058501d098fe7a1cb601c5ea05ae6d95ff1b303e
Niki Howe <nikihowe@gmail.com>
2022-08-29 14:58:39 -0400

    Add setup.py to allow running of jupyter notebooks outside IDEs

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='myriad',
+  packages=find_packages(),
+  license='Apache 2.0',
+  author='Nikolaus H. R. Howe, Simon Dufort-Labbé, Nitarshan Rajkumar',
+)
